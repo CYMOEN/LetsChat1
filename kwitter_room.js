ee9348b8edc5a5d1f03da0dead7370b4c5f3b171
CYMOEN <121981493+CYMOEN@users.noreply.github.com>
2023-05-04 18:37:30 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYh45EaqCzAROJqnFeK0okgu3F7iQuWYs",
+    authDomain: "chatterbox-34d14.firebaseapp.com",
+    databaseURL: "https://chatterbox-34d14-default-rtdb.firebaseio.com",
+    projectId: "chatterbox-34d14",
+    storageBucket: "chatterbox-34d14.appspot.com",
+    messagingSenderId: "940703225313",
+    appId: "1:940703225313:web:a6eccd9e873e17d95f6ea0",
+    measurementId: "G-Z5CN5RGZM9"
+  };
